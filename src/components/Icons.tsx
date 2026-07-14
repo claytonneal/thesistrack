b72e6941e175096b48808533e8e624f78fa46b00
clayton neal <clayton.neal@vechain.org>
2026-07-14 16:21:45 +0100

    trashcan design

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -26,7 +26,11 @@ export function PlusIcon({ className }: IconProps) {
 export function TrashIcon({ className }: IconProps) {
   return (
     <svg className={className} viewBox="0 0 24 24">
-      <path d="M4 7h16M9 7V5a1 1 0 0 1 1-1h4a1 1 0 0 1 1 1v2m-9 0 1 13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1l1-13" />
+      <path d="M3 6h18" />
+      <path d="M8 6V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2" />
+      <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6" />
+      <path d="M10 11v6" />
+      <path d="M14 11v6" />
     </svg>
   );
 }
